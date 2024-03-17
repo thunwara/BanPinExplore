40861c58194736d9fbbbe6f1d5e3a42a01e018b7
Thunwara <mtheewara@gmail.com>
2024-03-17 18:37:34 +0700

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
+
+const Page = () => {
+  return (
+    <View>
+      <Link href={"/(modals)/login"}>
+        Login
+      </Link>
+      <Link href={"/(modals)/booking"}>
+        Booking
+      </Link>
+      <Link href={"/listing/1337"}>
+        Listing detail
+      </Link>
+    </View>
+  )
+}
+
+export default Page
