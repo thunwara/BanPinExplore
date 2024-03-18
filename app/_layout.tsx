80fc3ee525b3ecf099c582e6471f34841236557e
Thunwara <mtheewara@gmail.com>
2024-03-18 16:27:14 +0700

    adding expo clerk, google login, not completed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,39 @@
-import { Ionicons } from '@expo/vector-icons';
-import { useFonts } from 'expo-font';
-import { Stack, useRouter } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { Ionicons } from "@expo/vector-icons";
+import { useFonts } from "expo-font";
+import { Stack, useRouter } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { TouchableOpacity } from "react-native";
+import * as SecureStore from "expo-secure-store";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
@@ -20,9 +41,9 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    'mon': require('../assets/fonts/Montserrat-Regular.ttf'),
-    'mon-sb': require('../assets/fonts/Montserrat-SemiBold.ttf'),
-    'mon-b': require('../assets/fonts/Montserrat-Bold.ttf'),
+    mon: require("../assets/fonts/Montserrat-Regular.ttf"),
+    "mon-sb": require("../assets/fonts/Montserrat-SemiBold.ttf"),
+    "mon-b": require("../assets/fonts/Montserrat-Bold.ttf"),
   });
 
   // Expo Router uses Error Boundaries to catch errors in the navigation tree.
@@ -44,41 +65,45 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-
+  // const { isLoaded, isSignedIn } = useAuth();
   const router = useRouter();
+
   return (
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-         name="(modals)/login"
-         options={{
-          title: 'Log in or sign up',
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(modals)/login"
+        options={{
+          title: "Log in or sign up",
           headerBackTitleStyle: {
-            fontFamily: 'mon-sb'
+            fontFamily: "mon-sb",
           },
-          presentation: 'modal',
+          presentation: "modal",
           headerLeft: () => (
-            <TouchableOpacity onPress={()=> router.back()}>
-              <Ionicons name='close-outline' size={28}/>
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
             </TouchableOpacity>
-          )
-         }}/>
-         <Stack.Screen
-         name="listing/[id]"
-         options={{
-          headerTitle: ''
-         }}/>
-         <Stack.Screen
-         name="(modals)/booking"
-         options={{
-          presentation: 'transparentModal',
-          animation: 'fade',
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="listing/[id]"
+        options={{
+          headerTitle: "",
+        }}
+      />
+      <Stack.Screen
+        name="(modals)/booking"
+        options={{
+          presentation: "transparentModal",
+          animation: "fade",
           headerLeft: () => (
-            <TouchableOpacity onPress={()=> router.back()}>
-              <Ionicons name='close-outline' size={28}/>
+            <TouchableOpacity onPress={() => router.back()}>
+              <Ionicons name="close-outline" size={28} />
             </TouchableOpacity>
-          )
-         }}/>
-      </Stack>
+          ),
+        }}
+      />
+    </Stack>
   );
 }
